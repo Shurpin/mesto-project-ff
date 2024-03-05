6f24d800b629e8eb66343b7b527d037c4c246357
Ivan Shurpin <ivanshurpin@yandex.ru>
2024-03-05 23:35:23 +0300

    добавил карточки и могу удалить одну

--- scripts/index.js
+++ scripts/index.js
@@ -4,19 +4,29 @@
 const container = document.querySelector('.places'); 
 const cardContainer = container.querySelector('.places__list');
 // @todo: Функция создания карточки
-function addcard() {
+function addcard(listItem) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  cardElement.querySelector('.card__image').src = "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg";
-  cardElement.querySelector('.card__title').textContent = 'titleValue';
+  cardElement.querySelector('.card__image').src = listItem.link;
+  cardElement.querySelector('.card__title').textContent = listItem.name;
   cardContainer.append(cardElement);
-}
+ }
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
-addcard();
 
+initialCards.forEach(function (item) {
+  addcard(item);
+});
 
+const resetButton = container.querySelector('.card__delete-button');
 
+resetButton.addEventListener('click', function () {
+  const cardRemove = document.querySelector('.card')
+  cardRemove.remove();
+  for (let i = 0; i < cardRemove.length; i++) {
+    cardRemove[i].remove();
+  }
+});
 
