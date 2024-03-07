c9ca6ab4dc53dd1b92db01fd7a5cd5b72c84ed58
Ivan Shurpin <ivanshurpin@yandex.ru>
2024-03-07 23:24:17 +0300

    finish

--- scripts/index.js
+++ scripts/index.js
@@ -4,32 +4,30 @@
 const container = document.querySelector('.places'); 
 const cardContainer = container.querySelector('.places__list');
 
-
+// @todo: Функция удаления карточки
+function removeCard(cardElement) {
+  cardElement.remove();
+};
 
 // @todo: Функция создания карточки
-function addcard(listItem) {
+function addCard(listItem) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   cardElement.querySelector('.card__image').src = listItem.link;
   cardElement.querySelector('.card__image').alt = listItem.name;
   cardElement.querySelector('.card__title').textContent = listItem.name;
  
-
   const deleteButton = cardElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', function () {
-    console.log('Мы кликнули по элементу');
-    cardElement.remove();
+  removeCard(cardElement);
   }); 
-  
 
   cardContainer.append(cardElement);
  }
 
-// @todo: Функция удаления карточки
-
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach(function (item) {
-  addcard(item);
+  addCard(item);
 });
 
